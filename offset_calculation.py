82021162b8a8cb60399d3dc8c1be365f0c52d8de
mechanical-girl <struan@duncan-wilson.co.uk>
2024-04-05 13:27:22 +0100

    Reformat data code

--- offset_calculation.py
+++ offset_calculation.py
@@ -1,4 +1,12 @@
-with open("output.py", "r") as f:
+with open("output.txt", "r") as f:
     filelines = f.read().split("\n")
 
+processed_data = {}
+for line in filelines:
+    angle, force = line.split(",")
+
+    if angle not in processed_data.keys(): # This is the first time this has angle has been encountered
+        processed_data[angle] = [force]
+    else: # The array exists and we can append to it
+        processed_data[angle].append(force)
 
